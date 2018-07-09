6b67430350be9da1addfe1da0bea35ce087658dc
PaulKerby <paulkerby@gmail.com>
2018-07-09 21:20:22 +0100

    Dynamic Graph Test

--- main.py
+++ main.py
@@ -1,1 +1,103 @@
+from __future__ import unicode_literals
 import sys
+import os
+import random
+import matplotlib
+
+matplotlib.use('Qt5Agg')
+from PyQt5 import QtCore, QtWidgets
+
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+progname = os.path.basename(sys.argv[0])
+progversion = "0.1"
+
+temps = []
+temp_x = []
+
+class MyMplCanvas(FigureCanvas):
+	def __init__(self, parent=None, width=5, height=4, dpi=100):
+		fig = Figure(figsize=(width, height), dpi=dpi)
+		self.axes = fig.add_subplot(111)
+
+		self.compute_initial_figure()
+
+		FigureCanvas.__init__(self, fig)
+		self.setParent(parent)
+
+		FigureCanvas.setSizePolicy(self,
+								   QtWidgets.QSizePolicy.Expanding,
+								   QtWidgets.QSizePolicy.Expanding)
+		FigureCanvas.updateGeometry(self)
+
+	def compute_initial_figure(self):
+		pass
+
+
+class MyDynamicMplCanvas(MyMplCanvas):
+	def __init__(self, *args, **kwargs):
+		MyMplCanvas.__init__(self, *args, **kwargs)
+		timer = QtCore.QTimer(self)
+		timer.timeout.connect(self.update_figure)
+		timer.start(1000)
+
+	def compute_initial_figure(self):
+		pass
+
+	def update_figure(self):
+		
+		temps.append( random.randint(18,22) )
+		temp_x.append( len(temp_x) + 1 )
+		
+		self.axes.cla()
+		self.axes.plot(temp_x, temps, 'r')
+		self.draw()
+
+
+class ApplicationWindow(QtWidgets.QMainWindow):
+	def __init__(self):
+		QtWidgets.QMainWindow.__init__(self)
+		self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+		self.setWindowTitle("application main window")
+
+		self.file_menu = QtWidgets.QMenu('&File', self)
+		self.file_menu.addAction('&Quit', self.fileQuit,
+								 QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
+		self.menuBar().addMenu(self.file_menu)
+
+		self.help_menu = QtWidgets.QMenu('&Help', self)
+		self.menuBar().addSeparator()
+		self.menuBar().addMenu(self.help_menu)
+
+		self.help_menu.addAction('&About', self.about)
+
+		self.main_widget = QtWidgets.QWidget(self)
+
+		l = QtWidgets.QVBoxLayout(self.main_widget)
+		dc = MyDynamicMplCanvas(self.main_widget, width=5, height=4, dpi=100)
+		l.addWidget(dc)
+
+		self.main_widget.setFocus()
+		self.setCentralWidget(self.main_widget)
+
+		self.statusBar().showMessage("Brew", 2000)
+
+	def fileQuit(self):
+		self.close()
+
+	def closeEvent(self, ce):
+		self.fileQuit()
+
+	def about(self):
+		QtWidgets.QMessageBox.about(self, "About", "Brewing" )
+
+		
+qApp = QtWidgets.QApplication(sys.argv)
+
+aw = ApplicationWindow()
+aw.setWindowTitle("%s" % progname)
+aw.show()
+sys.exit(qApp.exec_())
+#qApp.exec_()
